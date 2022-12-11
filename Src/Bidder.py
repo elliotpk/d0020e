9978b45b3608f2f52ff6895a6207b57c23b6f77a
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2022-12-11 18:52:57 +0100

    Added name to a bidder

--- Src/Bidder.py
+++ Src/Bidder.py
@@ -2,8 +2,9 @@
 import Behaviour
 
 class Bidder:
-  def __init__(self, id, amount, needs, behaviour, marketPrice):
+  def __init__(self, id, name, amount, needs, behaviour, marketPrice):
     self.id = id
+    self.name = name
     self.initAmount = amount
     self.currentAmount = amount
     self.needs = needs
@@ -29,9 +30,10 @@ class Needs:
 
 # Demonstration:
 
-bidder1 = Bidder(1, 150000, Needs(55, "steel beam"), Behaviour.A, 15000)
+bidder1 = Bidder(1, "John Andersson", 150000, Needs(55, "steel beam"), Behaviour.A, 15000)
 
 print("Created: Bidder ",bidder1.id,"\n",
+      "name: ",bidder1.name,"\n",
       "amount: ",bidder1.initAmount,"\n",
       "needs (amount): ",bidder1.needs.amount,"\n",
       "needs (type): ",bidder1.needs.type,"\n",
@@ -51,6 +53,7 @@ else:
   bidder1.setCurrentAmount(50000)
 
 print("Updated: Bidder ",bidder1.id,"\n",
+      "name: ",bidder1.name,"\n",
       "amount: ",bidder1.currentAmount,"\n",
       "needs (amount): ",bidder1.needs.amount,"\n",
       "needs (type): ",bidder1.needs.type,"\n",
