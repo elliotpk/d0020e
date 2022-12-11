5b8090c9520d00e66d12a91d38ae398cfda04bc8
elliotpk <elliot_pk@hotmail.com>
2022-12-11 02:35:41 +0100

    Testing some

--- Src/APILink.py
+++ Src/APILink.py
@@ -5,4 +5,18 @@
 #Getdata
 
 #Send to auction
+import requests
 
+#Module for sending API requests to the negotiation engine.
+
+API_URL = "http://127.0.0.1:5000"
+
+#Create Room (POST)
+
+#Auction Query (GET)
+
+#Join auction (GET)
+
+#Auction room (GET)
+
+#Auction room (POST)
