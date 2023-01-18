7ea37fba380a014f39db679a08ff28a806bb70d3
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2023-01-18 16:06:08 +0100

    #12 #14 Added more types of behaviour and tests
    Changed some variable names.
    Added more types of behaviours together with some testing in Bidder.py.

--- Src/Behaviour.py
+++ Src/Behaviour.py
@@ -1,13 +1,37 @@
 #Library, module
 
+# Desperate behaviour, always bids max amount.
+# Can bid for more than the market value, but not over the maximum amount.
 A = {
-  "bidMaxAmount": False,
-  "bidUpTo": 100000,
-  "onlyBidDiscounts": False
-} 
+  "onlyBidMaxAmount": True,
+  "bidMax": 0,
+  "discounts": {
+    "bidOnDiscounts": False,
+    "onlyBidOnDiscounts": False,
+    "minimumDiscount": 0.0
+  },
+  "bid": lambda price, marketPrice, bidMax:
+         # Can't bid over budget
+         False if (price > bidMax) else
+         # Can bid over market value
+         True if (price > marketPrice) else 
+         (price < bidMax)
+}
 
+# Bids on everything, including discounts of atleast 30% discount.
+# Doesn't bid for more than the market value or over the maximum amount.
 B = {
-  "bidMaxAmount": False,
-  "bidUpTo": 150000,
-  "onlyBidDiscounts": True
+  "onlyBidMaxAmount": False,
+  "bidMax": 0,
+  "discounts": {
+    "bidOnDiscounts": True,
+    "onlyBidOnDiscounts": False,
+    "minimumDiscount": 0.3
+  },
+  "bid": lambda price, marketPrice, bidMax:
+         # Can't bid over budget
+         False if (price > bidMax) else 
+         # Can't bid over market value
+         False if (price > marketPrice) else
+         (price < bidMax)
 } 
