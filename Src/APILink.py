e9370e4fde85bced2b73b8b5eb8c78a2144b840b
elliotpk <elliot_pk@hotmail.com>
2023-01-23 17:53:56 +0100

    Completed function for creating auction rooms

--- Src/APILink.py
+++ Src/APILink.py
@@ -7,11 +7,15 @@
 #Send to auction
 
 import requests
+from datetime import datetime, timedelta
+import json
 
 #Module for sending API requests to the negotiation engine.
 
-# Perhaps import this from the config file for ease of use?
+# Perhaps import this from the config file?
 API_URL = "http://127.0.0.1:5000"
+TIME_OUT = 3                            # Seconds
+ROOM_DURATION = 5                       # Minutes
 
 #Create Room (POST)
 #   To create the rooms(auctions) for the different blocks, done at init step of simulation
@@ -44,11 +48,41 @@ datad = {
     'ongoing':'True'
 }
 
-r = requests.get(API_URL+"/myrooms/admin", auth=('Elliot', ''), timeout=3) 
+#r = requests.get(API_URL+"/myrooms/admin", auth=('Elliot', ''), timeout=3) 
 #r = requests.post(API_URL+"/create-room", auth=('Elliot',''), data=datasd)
 
-print(r.text)
-if r:
-    print("yep")
-else:
-    print("nope")
+#def startAuction():
+
+#def checkWinner():
+
+#def placeBids(room_id):
+    #command = "/rooms/" + room_id
+    #required payload: username, message_input (bid amount), 
+    #requests.post()....
+    #catch 404
+  
+def createAuction(room_name:str, username:str, quantity:int):
+    "Posts an auction to the database and returns the Room ID if successful, otherwise None"
+
+    current_time = datetime.now() + timedelta(minutes=ROOM_DURATION)
+    payload = {
+        'room_name':room_name,
+        'privacy':'public',
+        'members':'',
+        'highest_bid':'',
+        'auction_type':'Ascending',
+        'closing_time':current_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        'reference_sector':'N/A',
+        'reference_type':'N/A',
+        'quantity':str(quantity),
+        'templatetype':'article',
+        'articleno':'N/A'
+    }
+    
+    r = requests.post(API_URL+"/create-room", auth=(username, ''), timeout=TIME_OUT, data=payload)
+    if(r.status_code == 200):
+        json_obj = r.json()
+        out = str(json_obj["message"])
+        return out.split("id: ",1)[1]
+    else:
+        return None
