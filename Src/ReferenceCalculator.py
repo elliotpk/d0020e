de14ad971fdfef1738e31920170854bf8bea6ccb
axelrinaldo <55086778+axelrinaldo@users.noreply.github.com>
2023-01-25 02:19:20 +0100

    2.1 Mk1 (w bugs) done & 2.2 started

--- Src/ReferenceCalculator.py
+++ Src/ReferenceCalculator.py
@@ -9,16 +9,21 @@
 
 from itertools import combinations
 from itertools import permutations
+from Block import *
+from Bidder import Bidder
 
 # blocklist: All the blocks together in a list (since the linked block class is not yet finished. Either one later changes the code to use these instead or adds a funtion to convert the linked list to an ordinary one)
 # buyers: A list of buyers.
 def referenceCalculator(blocklist, buyers):
     bestRajJainCostFairness = -1
-    PermNum = 0 # For testing
+    bestData = []
+    permNum = 0 # For testing
+    permAndComb = 0
+    
     
     for permutation in permutations(blocklist): # Will be performance intensive, consider more effective way if performance becomes a problem
         for combination in splitfinder(permutation, len(buyers)):
-            if(validCombination(combination)):
+            if(validCombination(combination, buyers)):
                 averageCostsForBuyers = []
                 i = 0
                 while i < len(combination)-1: # Runs through what the buyers have bought (the last index is the "unboughts")
@@ -28,9 +33,16 @@ def referenceCalculator(blocklist, buyers):
                 RajJainCostFairness = rajJainFairness(averageCostsForBuyers)
                 if (RajJainCostFairness > bestRajJainCostFairness):
                     bestRajJainCostFairness = RajJainCostFairness
-                    
-        print("Permutation", PermNum, "/", len(permutation))
+                    bestData.append(averageCostsForBuyers)
+                    i = 0
+                    while i < len(combination):
+                        bestData.append(combination[i])
+                        i = i + 1
+            permAndComb = permAndComb + 1
+        permNum = permNum + 1  # For testing
+        print("Permutation number ", permNum, "Combination number ", permAndComb)
         
+    print(bestData)    # For testing 
     return bestRajJainCostFairness
         
     #splitfinder to find all combinations of all permutations 
@@ -47,34 +59,34 @@ def referenceCalculator(blocklist, buyers):
     
 
 # Find all possible ways to split the blocks between the buyers (including one "buyer" for unbought blocks)
+# Assumes that only the "unbought" blocks can be empty since every buyer should have a demand
 def splitfinder(blocklist, numBuyers):
     
     possibleSplits = []
-    for breakpoints in combinations(range(1, len(blocklist)), numBuyers):  # Combinatorics: find all places to place splitpoints between groups
+    for breakpoints in combinations(range(1, len(blocklist)+1), numBuyers):  # Combinatorics: find all places to place splitpoints between groups
         possibilityN = []
         
         possibilityN.append(list(blocklist[0:breakpoints[0]]))
         for i in range(0, numBuyers-1):
             possibilityN.append(list(blocklist[breakpoints[i]:breakpoints[i+1]]))
-        possibilityN.append(blocklist[breakpoints[(len(breakpoints)-1)]:(len(blocklist))])
+        possibilityN.append(list(blocklist[breakpoints[(len(breakpoints)-1)]:(len(blocklist))]))
         
         possibleSplits.append(list(possibilityN))
-    
     return possibleSplits
 
 # Takes in a way to split the blocks between the buyers and returns whether what they have bought is allowed and whether their demand is fullfilled
-def validCombination(possibleCombination, buyers):
+def validCombination(Combination, buyers):
+    possibleCombination = Combination.copy() # To prevent the original list from being changed through references
     unboughts = possibleCombination.pop()
-    
     for unboughtBlock in unboughts:
         if(checkIfPreviousBlockUnbought(unboughtBlock, possibleCombination) == False):
             return False
     
     buyerIndex = 0
     for buyerBoughts in possibleCombination:
-        demand = buyers[buyerIndex].needs.amount
+        demand = buyers[buyerIndex].needs
         for block in buyerBoughts:
-            demand = demand - block.amount
+            demand = demand - block.get_amount()
         if(demand > 0):
             return False
     buyerIndex = buyerIndex + 1
@@ -83,12 +95,12 @@ def validCombination(possibleCombination, buyers):
     
 # Recursively check if a block that has to be bought after another block has been bought without this being the case
 def checkIfPreviousBlockUnbought(unboughtBlock, boughtBlocks):    
-    if(unboughtBlock.next != None):
+    if(unboughtBlock.next() != None):
         for blockset in boughtBlocks:
-            if(unboughtBlock.next in blockset):
+            if(unboughtBlock.next() in blockset):
                 return False
             else:
-                return checkIfPreviousBlockUnbought(unboughtBlock.next, boughtBlocks)
+                return checkIfPreviousBlockUnbought(unboughtBlock.next(), boughtBlocks)
     else:
         return True
     
@@ -109,46 +121,98 @@ def rajJainFairness(metricList):
 # Feed this function a valid set of blocks for a seller to buy and it will return the average cost paid
 def averageCost(boughtBlocks):
     sum = 0
+    totAmount = 0
     for block in boughtBlocks:
         discount = findDiscount(block,boughtBlocks)
-        sum = sum + (block.price - discount)
-    return sum
+        sum = sum + (block.get_amount() * (block.get_price() - discount))
+        totAmount = totAmount + block.get_amount()
+    return sum / totAmount
 
 # Finds the discount that should be applied to a given block recursivly under the assumption that block.discount is the cumulative discount on a base price given all previous blocks are bought 
 # Block = Block discount is sought for, boughtBlocks = The blocks bought by a buyer, foundPrevBlocks = recursion variable, init at 0
 # Goes back to the beginning of the sellers blockchain, finding how many blocks in that chain have been purchased. Then goes to the "index" in that blockchain that 
 # corresponds to the number of blocks found, there finding the discount of if the block had been moved to that place in the chain.
 def findDiscount(block, boughtBlocks, foundPrevBlocks = 0):
-    if (block.prev != None):
-        if(block.prev in boughtBlocks):
+    if (block.prev() != None):
+        if(block.prev() in boughtBlocks):
             foundPrevBlocks = foundPrevBlocks + 1
-        return findDiscount(block.prev, boughtBlocks, foundPrevBlocks)
+        return findDiscount(block.prev(), boughtBlocks, foundPrevBlocks)
     else:
         discount = 0
         while foundPrevBlocks > 0:
-            discount = block.next.discount
-            block = block.next
+            discount = block.next().get_discount()
+            block = block.next()
             foundPrevBlocks = foundPrevBlocks - 1
         return discount
             
 
 def demo():
-    blocklist = ["block11", "block12", "block21", "block31"]
-    buyers = 2
-    #blocklist = ["block11", "block12", "block13", "block21", "block22", "block31"]
-    #buyers = 3
-    
-    splits = splitfinder(blocklist, buyers)
-    print("\nPossible combinations:")
-    for split in splits:
-        print(split)
-    
-    
-    demoMetrics = [10, 10, 10, 10, 10]    
-    #demoMetrics = [5.5, 10, 7.6, 3]
-    #demoMetrics = [121, 220, 150, 89, 95, 140, 190]
-    
-    print("\n Rai Jain's fairness index given the input", demoMetrics, "\n", rajJainFairness(demoMetrics), "\n")
 
+    # blocklist = ["block11", "block12", "block21", "block31"]
+    # buyers = 2
+    # blocklist = ["block11", "block12", "block13", "block21", "block22", "block31"]
+    # buyers = 3
+    
+    # splits = splitfinder(blocklist, buyers)
+    # print("\nPossible combinations:")
+    # for split in splits:
+    #      print(split)
+    
+    
+    # demoMetrics = [10, 10, 10, 10, 10]    
+    # #demoMetrics = [5.5, 10, 7.6, 3]
+    # #demoMetrics = [121, 220, 150, 89, 95, 140, 190]
+    
+    # print("\n Rai Jain's fairness index given the input", demoMetrics, "\n", rajJainFairness(demoMetrics), "\n")
+    
+    # Demo 2:
+    
+    block1 = AuctionBlock("block1")
+    block2 = AuctionBlock("block2")
+    block3 = AuctionBlock("block3")
+    block4 = AuctionBlock("block4")
+    block5 = AuctionBlock("block5")
+    
+    block1.set_amount(1)
+    block2.set_amount(2)
+    block3.set_amount(3)
+    block4.set_amount(4)
+    block5.set_amount(5)
+    
+    block1.set_price(5)
+    block2.set_price(5)
+    block3.set_price(5)
+    block4.set_price(5)
+    block5.set_price(5)
+    
+    block1.set_discount(0)
+    block2.set_discount(1)
+    block3.set_discount(2)
+    block4.set_discount(3)
+    block5.set_discount(4)
+    
+    block1.set_nextBlock(block2)
+    block2.set_nextBlock(block3)
+    block3.set_nextBlock(block4)
+    block4.set_nextBlock(block5)
+    
+    
+    block2.set_prevBlock(block1)
+    block3.set_prevBlock(block2)
+    block4.set_prevBlock(block3)
+    block5.set_prevBlock(block4)
+    
+    buyer1 = Bidder(0,"Buyer",0,3,0,0)
+    buyer2 = Bidder(0,"Buyer",0,5,0,0)
+    buyer3 = Bidder(0,"Buyer",0,8,0,0)
+    
+    blocklist = [block1,block2,block3,block4,block5]
+    buyerlist = [buyer1, buyer2]
+    #buyerlist = [buyer1, buyer2, buyer3] # Show returning -1 when no solution can be found
+    
+    print(referenceCalculator(blocklist, buyerlist))
+    
+    
 demo()
     
+# TODO: Firstly fix checkIfPreviousBlockUnbought not working properly, then do the rest
