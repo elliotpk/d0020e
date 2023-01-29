49e72b77825db2ae789dcc3ae658b9e3f49ef116
elliotpk <elliot_pk@hotmail.com>
2023-01-29 14:16:49 +0100

    Most crucial functions implemented. Works fine in preliminary testing

--- Src/APILink.py
+++ Src/APILink.py
@@ -1,10 +1,4 @@
-#this is a Class APILink
-
-#trycatch over all 200
-
-#Getdata
-
-#Send to auction
+# Library / Module containing necessary functions to communicate with the API, abstracted into this module to keep the rest of the simulation more clear.
 
 import requests
 from datetime import datetime, timedelta
@@ -17,65 +11,37 @@ API_URL = "http://127.0.0.1:5000"
 TIME_OUT = 3                            # Seconds
 ROOM_DURATION = 40                       # Minutes
 
-#Create Room (POST)
-#   To create the rooms(auctions) for the different blocks, done at init step of simulation
-#   Pass in an object containing the necessary info: room_name, (reference_sector, reference_type), quantity, (articleno, members) 
-
-#Auction Query (GET)
-    #Want to get the status of all currently running auctions
-
-#Join auction (GET)
-    #Perhaps not strictly needed? Or have it tied into the first time a bid is placed
-
-#Auction room (GET)
-
-#Auction room (POST)
-datasd = {
-    'room_name':'test2',
-    'privacy':'public',
-    'members':'',
-    'highest_bid':'',
-    'auction_type':'Ascending',
-    'closing_time':'2022-12-11T10:00:00',
-    'reference_sector':'test',
-    'reference_type':'test',
-    'quantity':'1111',
-    'templatetype':'article',
-    'articleno':'22f'
-}
-
-datad = {
-    'ongoing':'True'
-}
-
-#r = requests.get(API_URL+"/myrooms/admin", auth=('Elliot', ''), timeout=3) 
-#r = requests.post(API_URL+"/create-room", auth=('Elliot',''), data=datasd)
-
-#def startAuction():
-
-#def checkWinner():
+def endAuction(room_id:str, username:str, winner:str):
+    "Used to decide on a winner for the auction, username needs to be the admin for the auction and winner the username of the person who won"
+    r = requests.post(API_URL+"/rooms/"+room_id+"/end", auth=(username, ''), data={"winner":winner})
+    return r.text
 
 def placeBid(room_id:str, username:str, value:int):
     "Places a bid to auction <room_id> with amount <value>, returns True if successful, otherwise False"
 
-    payload = {
-        'message_input':value
-    }
-
-    r = requests.post(API_URL+"/rooms/"+room_id, auth=(username, ''), timeout=TIME_OUT, data=payload)
+    r = requests.post(API_URL+"/rooms/"+room_id, auth=(username, ''), timeout=TIME_OUT, data={'message_input':value})
     if(r.status_code == 200):
         return True
     else:
         return False
 
-def getRoomInfo(room_id:str, username:str):
-    "Used to get all the auction bids placed in a specific auction room"
+def getRoomInfo(room_id:str, username:str, order:str):
+    """
+    Used to get all the auction bids placed in a specific auction room. 
+    Can order the list by time (order = 'time') or bid amount (order = 'bid')
+    """
     r = requests.get(API_URL+"/rooms/"+room_id, auth=(username, ''), timeout=TIME_OUT)
     json_obj = r.json()                                                                     
-    layer1 = str(json_obj["Bids"]).split(":", 10)                                           # Might be a weird method to get out the bidder name and amount
-    name = layer1[8].split("'", 2)[1]                                                       # but it worked alright in testing so should be good seeing as this function
-    value = layer1[10].split("'",2)[1]                                                      # may not even end up seeing use
-    return name + ", " + value
+    value = json_obj["Bids"]
+    print(value)
+    output = []
+    for entry in value:
+        output.append({"user" : entry["sender"]["val"][0], "value" : entry["text"]["val"][0]})
+    sort = False
+    if(order == "bid"):
+        sort = True
+
+    return sorted(output, key=lambda i:i["value"], reverse=sort)
 
 def addUser(room_id:str, username:str):
     r = requests.get(API_URL+"/rooms/"+room_id+"/join", auth=(username, ''), timeout=TIME_OUT)
