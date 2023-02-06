8c950d89f69cd20ca51965bf456af3b12a7a9821
axelrinaldo <55086778+axelrinaldo@users.noreply.github.com>
2023-02-06 17:16:40 +0100

    Comment out demo function

--- Src/Block.py
+++ Src/Block.py
@@ -66,12 +66,3 @@ class LinkOfBlocks:
             currentBlock = currentBlock.NextBlock
             Blocks.append(currentBlock.Object)
         print(Blocks)
-
-AuctionList = LinkOfBlocks()
-
-AuctionList.add("Guitars")
-AuctionList.add("Bears")
-
-AuctionList.display()
-
-print(AuctionList.get_size())
