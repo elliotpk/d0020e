17847579d854834aced7935f11b30d386bc1e33f
axelrinaldo <55086778+axelrinaldo@users.noreply.github.com>
2023-02-06 17:08:44 +0100

    Merge pull request #28 from elliotpk/feature_axel
    Feature merging to develop

--- Src/Behaviour.py
+++ Src/Behaviour.py
@@ -1,1 +1,13 @@
-#Liberary
+#Library, module
+
+A = {
+  "bidMaxAmount": False,
+  "bidUpTo": 100000,
+  "onlyBidDiscounts": False
+} 
+
+B = {
+  "bidMaxAmount": False,
+  "bidUpTo": 150000,
+  "onlyBidDiscounts": True
+} 
