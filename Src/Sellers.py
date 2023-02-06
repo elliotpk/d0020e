462914b240040d9181439d50b6641fa6a4887b2e
jasade <94181984+jasade@users.noreply.github.com>
2023-02-06 18:02:48 +0100

    merge dev

--- Src/Sellers.py
+++ Src/Sellers.py
@@ -5,7 +5,7 @@ class Sellers:
 
     def __init__(self,id):
         self.id = id
-        self.auctionId = self.createAuction(id)
+        self.auctionId = self.createAuction()
         self.LinkOfBlocks = Block.LinkOfBlocks()
 
     def createAuction(self):
