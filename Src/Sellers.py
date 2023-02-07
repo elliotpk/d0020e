eb14d5d5c017cbe37cf9830c2690994631f0fe31
Anfallsigelkott <axelrinaldo@gmail.com>
2023-02-07 17:59:40 +0100

    Couple refcalc to main

--- Src/Sellers.py
+++ Src/Sellers.py
@@ -19,12 +19,14 @@ class Sellers:
         self.LinkOfBlocks.head.set_price(price)
         self.LinkOfBlocks.head.set_amount(amount)
         self.LinkOfBlocks.head.set_discount(discount)
+        self.LinkOfBlocks.head.set_Object("Block")
 
     def addBlock(self,price,amount,discount):
         block=Block.AuctionBlock()
         block.set_price(price)
         block.set_amount(amount)
         block.set_discount(discount)
+        block.set_Object = "Block " + str(price) + " " + str(amount) + " " + str(discount) # For debugging mostly
         self.LinkOfBlocks.add(block)
 
 
