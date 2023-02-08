9220ee69936a49408671e31b9991a422f490e72b
elliotpk <elliot_pk@hotmail.com>
2023-02-08 17:10:03 +0100

    Working demo with simplified buyers

--- Src/Sellers.py
+++ Src/Sellers.py
@@ -1,35 +1,17 @@
 import APILink
-import random
-import Block
+#import random
+#import Block
 class Sellers:
 
-    def __init__(self,id):
+    def __init__(self,id,quantity):
         self.id = id
-        self.auctionId = None
-        self.LinkOfBlocks = Block.LinkOfBlocks()
+        self.quantity = quantity
+        self.auctionId = ''
 
     def createAuction(self):
-        try:
-            roomid=APILink.createAuction(str(self.id),"Seller",10)
-        except:
-            roomid=None
-            self.auctionId = roomid
+        roomid=APILink.createAuction(str(self.id),"Seller",self.quantity)
+        self.auctionId = roomid
         return roomid
 
-    def genBlock(self,price,amount,discount):
-        self.LinkOfBlocks.head.set_price(price)
-        self.LinkOfBlocks.head.set_amount(amount)
-        self.LinkOfBlocks.head.set_discount(discount)
-        self.LinkOfBlocks.head.set_Object("Block")
-
-    def addBlock(self,price,amount,discount):
-        block=Block.AuctionBlock()
-        block.set_price(price)
-        block.set_amount(amount)
-        block.set_discount(discount)
-        block.set_Object = "Block " + str(price) + " " + str(amount) + " " + str(discount) # For debugging mostly
-        self.LinkOfBlocks.add(block)
-
-
-
+    
 
